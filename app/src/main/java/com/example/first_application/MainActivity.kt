fa2677466d4cd83649fe512d24c5e152ea481361
Klissman20 <klesteban0.3@gmail.com>
2020-04-19 17:03:42 -0500

    Sistema de domotica version 0.1

--- app/src/main/java/com/example/first_application/MainActivity.kt
+++ app/src/main/java/com/example/first_application/MainActivity.kt
@@ -10,31 +10,39 @@ import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
 
+    //Crear instancia de la base de datos  Firebase y referenciar el arbol "home"
     val database = FirebaseDatabase.getInstance()
+
+    //se crea la referencia para apuntar al dato que se requiere en la base de datos.
     val refHome = database.getReference("home")
+    val refLuces: DatabaseReference = refHome.child("luces")
+    val refLuz_sala: DatabaseReference = refLuces.child("luz_sala")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //referencia del boton toggle
         val btntg = findViewById<ToggleButton>(R.id.toggle)
 
-        val refLuces: DatabaseReference = refHome.child("luces")
-        val refLuz_sala: DatabaseReference = refLuces.child("luz_sala")
-
+        //Se ejecuta la funcion para cambiar el estado del dato apuntado.
         control_LED(refLuz_sala,btntg);
 
     }
 
+    //funcion para leer y modificar el estado de un dato en la base de datos firebase mediante boton
     private fun control_LED(refLED: DatabaseReference, TgButton: ToggleButton): Unit {
 
+        //Si el boton es presionado, debe registrarse el valor actual en la base de datos
         TgButton.setOnCheckedChangeListener { buttonView, isChecked ->
             refLED.setValue(isChecked)
         }
 
+        //Evento de escucha para cambios de estado del dato referenciado
         refLED.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 val estado_led = dataSnapshot.value as Boolean
+                //Si el estado cambia, el boton toggle tambien cambia su estado en la app
                 TgButton.setChecked(estado_led)
                 if (estado_led) {
                     TgButton.setTextOn("APAGAR")
@@ -42,7 +50,7 @@ class MainActivity : AppCompatActivity() {
                     TgButton.setTextOff("ENCENDER")
                 }
             }
-
+            
             override fun onCancelled(databaseError: DatabaseError) {}
         })
     }
